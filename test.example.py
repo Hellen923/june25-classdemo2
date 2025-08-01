8a479bf6b443c8045f121809cbedb4d13a9d7234
Kiwagama Hellen <hellenkiwagama@gmail.com>
2025-08-01 10:45:01 +0300

    added test_example.py

--- test.example.py
+++
@@ -1,6 +0,0 @@
-def test_addition():
-    assert 1 + 1 == 2
-
-# Remove or comment out this failing test unless you want to test failure
-# def test_failure():
-#     assert 1 + 1 == 3  # This will fail
